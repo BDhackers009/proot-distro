0137679629b4b2f6b9e2e7a14fa3d97613d248da
Mustakim Ahmed Sifat <67186139+BDhackers009@users.noreply.github.com>
2022-05-02 05:51:21 +0600

    Update kali.sh

--- distro-plugins/kali.sh
+++ distro-plugins/kali.sh
@@ -3,4 +3,8 @@ TARBALL_URL['aarch64']="https://kali.download/nethunter-images/current/rootfs/ka
 TARBALL_SHA256['aarch64']="2aa9e8b1d7561768abd1e2ce8104f1ccc60cbf3072b58e976edf219114808546"
 TARBALL_URL['arm']="https://kali.download/nethunter-images/current/rootfs/kalifs-armhf-nano.tar.xz"
 TARBALL_SHA256['arm']="9ea566808762960b4f24982f7c0b41da49b6b86417d83fa6573f74feb97e0f0a"
+distro_setup() {
+	run_proot_cmd userdel -rf kali
+	run_proot_cmd touch ~/hushlogin
+}
 
