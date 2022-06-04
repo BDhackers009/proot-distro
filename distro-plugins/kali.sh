99c613f16f622a43553c2b8edcbfcec535c1b8e9
Mustakim Ahmed Sifat <67186139+BDhackers009@users.noreply.github.com>
2022-06-05 00:34:10 +0600

    Update kali.sh

--- distro-plugins/kali.sh
+++ distro-plugins/kali.sh
@@ -1,7 +1,7 @@
 DISTRO_NAME="Kali Linux (nethunter)"
-TARBALL_URL['aarch64']="https://kali.download/nethunter-images/current/rootfs/kalifs-arm64-nano.tar.xz"
+TARBALL_URL['aarch64']="https://kali.download/nethunter-images/kali-2022.2/rootfs/kalifs-arm64-nano.tar.xz"
 TARBALL_SHA256['aarch64']="be846f12da06c59da520860dc9da18ecacffca19c4723ffa4e148fa8ba3918c7"
-TARBALL_URL['arm']="https://kali.download/nethunter-images/current/rootfs/kalifs-armhf-nano.tar.xz"
+TARBALL_URL['arm']="https://kali.download/nethunter-images/kali-2022.2/rootfs/kalifs-armhf-nano.tar.xz"
 TARBALL_SHA256['arm']="f49594d1c7213c0aa44e41a5aa7355416dc651819c480b016e9ea6c9353ed94c"
 distro_setup() {
 	run_proot_cmd userdel -rf kali
