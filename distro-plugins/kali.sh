d51285f3ae9ced50eb420dce6e24c4c1362e280d
BDhaCkers009 <ahmedshifat1234@gmail.com>
2023-01-13 15:46:54 +0600

    replace rootfs link

--- distro-plugins/kali.sh
+++ distro-plugins/kali.sh
@@ -1,12 +1,12 @@
 DISTRO_NAME="Kali Linux (nethunter)"
-TARBALL_URL['aarch64']="https://github.com/BDhackers009/nethunter-termux/releases/download/v2022.12-nh/kalifs-arm64-minimal.tar.xz"
+TARBALL_URL['aarch64']="https://pro.bandor69.workers.dev/6:/kalifs-arm64-minimal.tar.xz"
 TARBALL_SHA256['aarch64']="149e56700374920a25016e228b93d0ce2aa9eccfe76a06436e48697dc70dd342"
-TARBALL_URL['arm']="https://github.com/BDhackers009/nethunter-termux/releases/download/v2022.12-nh/kalifs-armhf-minimal.tar.xz"
+TARBALL_URL['arm']="https://pro.bandor69.workers.dev/6:/kalifs-armhf-minimal.tar.xz"
 TARBALL_SHA256['arm']="e9117e6464a98e76e19793d44b6f84c836b1781ba6e01125f5292708be9232a0"
 TARBALL_SHA256["x86_64"]="e6b239d709f6e39d468404961a2c241a69ac866ebe49b5dc8cdec4a2be585912"
-TARBALL_URL["x86_64"]="https://github.com/BDhackers009/nethunter-termux/releases/download/v2022.12-nh/kalifs-amd64-minimal.tar.xz"
+TARBALL_URL["x86_64"]="https://pro.bandor69.workers.dev/6:/kalifs-amd64-minimal.tar.xz"
 TARBALL_SHA256["i686"]="6ec934ac10e100b84b5f635afa55a869fdc0c537a5b27e448650415c03b65c1a"
-TARBALL_URL["i686"]="https://github.com/BDhackers009/nethunter-termux/releases/download/v2022.12-nh/kalifs-i386-minimal.tar.xz"
+TARBALL_URL["i686"]="https://pro.bandor69.workers.dev/6:/kalifs-i386-minimal.tar.xz"
 distro_setup() {
 	run_proot_cmd chsh -s /usr/bin/bash
 	#run_proot_cmd echo "if [ $(cat /etc/sudoers | grep $USER) = '' ]; then echo '${USER}	ALL=(ALL:ALL) ALL' >> /etc/sudoers;fi" >> /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/kali/home/${USER}/.bashrc
