47b044471cab2fbedb18ba021d554cf6845bdcde
Mustakim Ahmed Sifat <67186139+BDhackers009@users.noreply.github.com>
2023-08-25 10:51:35 +0600

    Update kali.sh

--- distro-plugins/kali.sh
+++ distro-plugins/kali.sh
@@ -1,11 +1,11 @@
 DISTRO_NAME="Kali Linux (nethunter)"
 TARBALL_URL['aarch64']="https://kali.download/nethunter-images/current/rootfs/kalifs-arm64-nano.tar.xz"
-TARBALL_SHA256['aarch64']="04a12b79d4ac38b7c85ea3eb25a85bff07fb89505a889782a114a97a5de2a84b"
+TARBALL_SHA256['aarch64']="5d4f415ec7793a35df24b9cd17eded794294a8edf135f7915c61dd3b3143100f"
 TARBALL_URL['arm']="https://kali.download/nethunter-images/current/rootfs/kalifs-armhf-nano.tar.xz"
-TARBALL_SHA256['arm']="17bae20743c67017d83448bc82de7cedaeb354d1c75dbadf2357fe93cf769f84"
-TARBALL_SHA256["x86_64"]="b7cfeccdcbf7a2844245405ecc680432237d3b0da49ee35eab0f3ce9fab645a1"
+TARBALL_SHA256['arm'] = "ec8644b1bb8b7780c4632be0a4410b5be9b8ae0c54c3ffa3eba70912f1b72c76"
+TARBALL_SHA256["x86_64"]="0a256e9c3bd84e46e9f10c2f7563511eb69a6ae4edeaac3e1fdeb5f9058d111b"
 TARBALL_URL["x86_64"]="https://kali.download/nethunter-images/current/rootfs/kalifs-amd64-minimal.tar.xz"
-TARBALL_SHA256["i686"]="0dd4b95635b601b9516c29bee016363dd5f378f9abb104736f93f76c82c3d8b6"
+TARBALL_SHA256["i686"]="1d3fec3d761c7a397f4d6d87a323e2025fb2e880bcf09e28bc551e4fbcac627a"
 TARBALL_URL["i686"]="https://kali.download/nethunter-images/current/rootfs/kalifs-i386-minimal.tar.xz"
 distro_setup() {
 	run_proot_cmd chsh -s /usr/bin/bash
